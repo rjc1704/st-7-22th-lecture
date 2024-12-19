daf07711231447a7366075b0549e47ce3dd6bb98
Jecheon Ryu <rjc1704@gmail.com>
2024-12-20 04:28:05 +0900

    일부수정

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,7 +1,5 @@
 import { Todo } from "@/types/todo.type";
 import { createClient } from "@/utils/supabase/server";
-import { Session } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   const supabase = createClient();
@@ -14,14 +12,14 @@ export async function PATCH(request: Request) {
   const supabase = createClient();
   const { id, completed } = (await request.json()) as {
     id: Todo["id"];
-    completed: Todo["isCompleted"];
+    completed: Todo["is_completed"];
   };
   const {
     data: { session },
   } = await supabase.auth.getSession();
   const { data, error } = await supabase
     .from("todos")
-    .update({ isCompleted: completed })
+    .update({ is_completed: completed })
     .eq("id", id)
     .select();
 
